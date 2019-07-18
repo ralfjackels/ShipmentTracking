7db97f9e9d07a3c24a4bded34cc4eca1ef88e90c
Martin Terens <martin.terens@gmail.com>
2019-07-18 15:20:47 +0200

    Enum erstellt

--- src/main/java/com/dhl_miniprojekt/model/Lieferstatus.java
+++ src/main/java/com/dhl_miniprojekt/model/Lieferstatus.java
@@ -3,7 +3,22 @@ package com.dhl_miniprojekt.model;
  * 
  */
 public enum Lieferstatus {
-    Aufgegeben,
-    Auslieferung,
-    Zugestellt
+
+    AUFGEGEBEN("Aufgegeben"), ZUGESTELLT("Zugestellt"), IN_ZUSTELLUNG("In Zustellung");
+
+    private String lieferstatus;
+
+    Lieferstatus(String lieferstatus) {
+        this.lieferstatus = lieferstatus;
+    }
+
+    public String getLieferstatus() {
+        return lieferstatus;
+    }
+
+    public void setLieferstatus(String lieferstatus) {
+        this.lieferstatus = lieferstatus;
+    }
+
+
 }
