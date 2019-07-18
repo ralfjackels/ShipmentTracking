9b7398296b9f851935abbb30447415e673c6f026
RJ <jackelsralf@gmail.com>
2019-07-18 15:22:49 +0200

    Controller Integer gesuchteSendung +++ Controller Konstruktor

--- src/main/java/com/dhl_miniprojekt/model/Sendung.java
+++ src/main/java/com/dhl_miniprojekt/model/Sendung.java
@@ -7,34 +7,30 @@ import java.util.*;
  */
 public class Sendung {
 
-    /**
-     * Default constructor
-     */
-    public Sendung() {
-    }
 
 
-    private int sendungNummer;
-
-
+    // Attribute
+    private Integer sendungNummer;
     private boolean standard;
-
-
     private  Adresse lieferAdresse;
-
-
     private Adresse startAdresse;
-
-
     private Lieferstatus lieferstatus;
-
-
     private LocalDateTime abgabeZeitpunkt;
-
-
     private LocalDateTime lieferZeitpunkt;
 
 
+    // Konstruktor
+    public Sendung() {
+    }
 
+    public Sendung(Integer sendungNummer, boolean standard, Adresse lieferAdresse, Adresse startAdresse, Lieferstatus lieferstatus, LocalDateTime abgabeZeitpunkt, LocalDateTime lieferZeitpunkt) {
+        this.sendungNummer = sendungNummer;
+        this.standard = standard;
+        this.lieferAdresse = lieferAdresse;
+        this.startAdresse = startAdresse;
+        this.lieferstatus = lieferstatus;
+        this.abgabeZeitpunkt = abgabeZeitpunkt;
+        this.lieferZeitpunkt = lieferZeitpunkt;
+    }
 
 }
